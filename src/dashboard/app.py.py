e71aa72e1e11a02bf41af714539676224a1fd7d8
Jose Andrey Prado Rojas <andrey.prado@quantosoluciones.com>
2025-11-18 09:54:18 -0600

    conflictos

--- src/dashboard/app.py.py
+++ src/dashboard/app.py.py
@@ -11,6 +11,9 @@ import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(CURRENT_DIR)
 df = pd.read_csv(r"../../data/clean/datos_limpios.csv")
+df = pd.read_csv(
+    r"../../data/clean/datos_limpios.csv"
+)
 df = df[df["total"] > 0]
 
 with open(
